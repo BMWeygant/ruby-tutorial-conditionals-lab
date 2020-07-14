2ebdb5a45a9b0083a12e2735d51bf960b3e1408d
BMWeygant <bobbydigital108@gmail.com>
2020-07-13 20:41:04 -0400

    Added solution.

--- conditionals.rb
+++ conditionals.rb
@@ -1,3 +1,12 @@
 def ghosts_audible(play) 
     #place your code inside here
-end     
+    if play == "qb sneak"
+        "Sam went from seeing ghosts to being a ghost after that qb sneak!"
+    elsif play == "rollout"
+        "Sam threw the ball away."
+    elsif play == "hb screen"
+        "Le'Veon Bell is a beast! TD Jets!"
+    else 
+        "Another int by Sam!"
+    end
+end
